672dd42c242782ff4c52184e79759051394cf8ee
BitSoftwareCo <74556793+BitSoftwareCo@users.noreply.github.com>
2020-12-06 13:32:08 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -2,11 +2,9 @@
 // Side bar
 //
 function openNav() {
-    document.getElementById("sidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("sideBar").style.width = "100%";
 }
-
+  
 function closeNav() {
-    document.getElementById("sidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("sideBar").style.width = "0%";
 }
